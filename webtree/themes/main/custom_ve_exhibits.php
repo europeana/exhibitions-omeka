de8e2dca2ba8d12555f2ab003bd4201d79a849a6
andyjmaclean@gmail.com <andyjmaclean@gmail.com>
2012-03-23 17:50:52 +0000

    test commit

--- webtree/themes/main/custom_ve_exhibits.php
+++ webtree/themes/main/custom_ve_exhibits.php
@@ -14,6 +14,8 @@ function ve_exhibit_builder_link_to_exhibit($exhibit, $text = null, $props = arr
 
 function ve_exhibit_builder_exhibit_display_item_info_link($linkProperties = array())
 {
+// test commit
+
     $item = get_current_item();
     $page = exhibit_builder_get_current_page();
     // pass along page param as this is not available from the item page but is necessary for building seo title and meta fields
