a729d878910fdbb39639506268fdaa027a6ae4ae
andyjmaclean@gmail.com <andyjmaclean@gmail.com>
2012-03-29 07:56:47 +0000

    ipad rotate fix

--- webtree/themes/main/common/header.php
+++ webtree/themes/main/common/header.php
@@ -48,6 +48,19 @@ ve_session_vars();
 	
 	<?php echo js('euresponsive'); ?>
 	<script type="text/javascript">
+
+		// ipad fix
+		if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) {
+		    var viewportmeta = document.querySelector('meta[name="viewport"]');
+		    if (viewportmeta) {
+		        viewportmeta.content = 'width=device-width, minimum-scale=1.0, maximum-scale=1.0, initial-scale=1.0';
+		        document.body.addEventListener('gesturestart', function () {
+		            viewportmeta.content = 'width=device-width, minimum-scale=0.25, maximum-scale=1.6';
+		        }, false);
+		    }
+		}
+			
+	
 		<?php
 		if(get_option('euresponsive_zoomit')){
 			$IMAGEWIDTHS = explode("~", get_option('euresponsive_imagewidths'));
