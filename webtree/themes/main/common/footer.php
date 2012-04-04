ed48463bb0b946847840aa33675733860e72b99c
andyjmaclean@gmail.com <andyjmaclean@gmail.com>
2012-04-04 12:49:13 +0000

    Addthis test 2

--- webtree/themes/main/common/footer.php
+++ webtree/themes/main/common/footer.php
@@ -11,7 +11,7 @@
 
 
 
-<div class="row" id="bottom-navigation">
+<div class="row" id="bottom-navigation"  style="background-color:red!important">
 	<div class="six columns">
 		<ul class="navigation">
 			<li>
@@ -42,42 +42,88 @@
 
 
         </div>
-        <div class="six columns" style="text-align: right;">
-           <ul class="navigation">
-            <!--   <li><a href="http://www.facebook.com/Europeana" target="_blank" title="Follow us on Facebook!"><img src="http://exhibitions.europeana.eu/themes/europeana/images/icon_Facebook.png" alt="Follow us on Facebook!"></a></li>
-               <li><a href="http://twitter.com/EuropeanaEU" target="_blank" title="Follow us on Twitter!"><img src="http://exhibitions.europeana.eu/themes/europeana/images/icon_Twitter.png" alt="Follow us on Twitter!"></a></li> -->
-               <li><!-- AddThis Button BEGIN -->
-<div class="addthis_toolbox addthis_default_style " style="float: right; display: inline; padding-left: 30px;">
-<a class="addthis_button_facebook_like" fb:like:layout="button_count"></a>
-<a class="addthis_button_tweet"></a>
-<a class="addthis_counter addthis_pill_style"></a>
-</div>
-</li>
-
-           </ul>
-
-           <script type="text/javascript">var addthis_config = {"data_track_clickback":true};</script>
-<script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=ra-4d70f66c15fff6d0">
-<!-- AddThis Button END -->
-            var addthis_config = {
-            ui_language: "en",
-            ui_click: true,
-            ui_cobrand: "Europeana"
-
-            }
-            </script>
+		<div class="six columns" style="text-align: right;">
+			<ul class="navigation">
+				<!--   <li><a href="http://www.facebook.com/Europeana" target="_blank" title="Follow us on Facebook!"><img src="http://exhibitions.europeana.eu/themes/europeana/images/icon_Facebook.png" alt="Follow us on Facebook!"></a></li>
+				<li><a href="http://twitter.com/EuropeanaEU" target="_blank" title="Follow us on Twitter!"><img src="http://exhibitions.europeana.eu/themes/europeana/images/icon_Twitter.png" alt="Follow us on Twitter!"></a></li> -->
+				<li><!-- AddThis Button BEGIN -->
  
-            <script type="text/javascript">
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-12776629-3']);
-  _gaq.push(['_trackPageview']);
+ 
+ <script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=xa-4f7c39d46532bd4d"></script>
+ 
+ 
+ 
 
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-</script>
+ 
+ 					<style type="text/css">
+ 					/*
+						.addthis_toolbox .at15t_compact {
+ 							margin-right: 0;
+						}
+ 					
+ 					*/
+ 					</style>
+ 
+						 
+					<div class="addthis_toolbox addthis_default_style " style="float:right; display: inline; padding-left: 30px;">
+						<a class="addthis_button_facebook"></a>
+						<a class="addthis_button_twitter"></a>
+						<a class="addthis_button_facebook_like"></a>
+						
+						<a class="addthis_button_google_plusone at300b" g:plusone:count="false" g:plusone:size="small"></a>
+						<!-- a class="addthis_button_google_plusone" g:plusone:annotation="bubble" g:plusone:size="small"></a--> 
+						
+						<a class="addthis_counter addthis_pill_style"></a>  <!--  "addthis_pill_style" -->
+					</div>
+  
+ <!-- 
+					<div class="addthis_toolbox addthis_default_style ">
+						<a class="addthis_button_preferred_1"></a>
+						<a class="addthis_button_preferred_2"></a>
+
+												<a class="addthis_button_preferred_4"></a>
+						<a class="addthis_button_compact"></a>
+						<a class="addthis_counter addthis_bubble_style"></a>
+					</div>
+  -->
+  
+  
+<script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=xa-4f7c39d46532bd4d"></script>
+<!-- AddThis Button END -->
+				</li>
+			</ul>
+	
+ 
+
+
+
+
+			<script type="text/javascript">
+	            var addthis_config = {
+	            	"pubid":				'xa-4b4f08de468caf36',	// ra-4d70f66c15fff6d0 
+		            "ui_language":			'en',
+		            "ui_click":				false,					// click to reveal men or show on  mouse over?
+		            "ui_cobrand":			'Europeana',
+					"data_track_clickback":	true,
+					"data_ga_tracker":		null					// Google Analytics tracking object, or the name of a global variable that references it. If set, we'll send AddThis tracking events to Google, so you can have integrated reporting.
+	            }
+	            
+			</script>
+			<script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=ra-4d70f66c15fff6d0"></script>
+
+
+						
+            <script type="text/javascript">
+			  var _gaq = _gaq || [];
+			  _gaq.push(['_setAccount', 'UA-12776629-3']);
+			  _gaq.push(['_trackPageview']);
+			
+			  (function() {
+			    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+			  })();
+			</script>
 
             
 
