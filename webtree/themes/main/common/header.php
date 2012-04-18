03d333673309ed06f93d1df52ef7e611656a54aa
andyjmaclean@gmail.com <andyjmaclean@gmail.com>
2012-04-18 15:05:49 +0000

    tidied doctype

--- webtree/themes/main/common/header.php
+++ webtree/themes/main/common/header.php
@@ -6,8 +6,6 @@ ve_session_vars();
 
 ?>
 
-<!--DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"-->
-
 <!doctype html>
 	<head>
 	<meta charset="utf-8" />
