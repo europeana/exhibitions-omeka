cd7393d21c286410d02c46410d30e5067dde9060
andyjmaclean@gmail.com <andyjmaclean@gmail.com>
2012-04-23 08:12:20 +0000

    Added hard copy of contact.php

---
+++ webtree/static/contact.php
@@ -0,0 +1,7 @@
+<h1>Contact</h1>
+
+<p>Do you have any feedback or comments about the exhibitions here. We'd love to hear from you, please send your email to the following address:</p>
+
+<a href="mailto:marcomms@europeana.eu" title="Virtual Exhibitions" target="_blank">Marcomms AT europeana.eu</a>
+
+</div>
