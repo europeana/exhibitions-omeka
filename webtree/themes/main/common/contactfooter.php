f23d226929bba679913ec974aacba2d37cc3510e
andyjmaclean@gmail.com <andyjmaclean@gmail.com>
2012-04-23 09:01:30 +0000

    Updated footer for credits page

--- webtree/themes/main/common/contactfooter.php
+++ webtree/themes/main/common/contactfooter.php
@@ -8,48 +8,36 @@
     $pageSlug = 'credits-' . $eName;
     $creditsPage = ve_get_page_by_slug($pageSlug);
 ?>
-</div><!-- end content  -->
-</div><!-- end container -->
 
-<div class="clear"></div>
+<div class="row" id="footer">
+	<div class="twelve columns" id="bottom-navigation">
+		<ul class="navigation">
+			<li>
+					<a href="<?php echo uri('contact');?>"><?php echo ve_translate('contact', 'Contact');?></a>
+			</li>
+			<?php if (exhibit_builder_get_current_exhibit()): ?>
+			<li>
+				<a class="return-to" rel="<?php echo uri(); ?>"
+					href="<?php echo uri('items/browse') . '/?tags=' . ve_get_exhibit_name_from_slug($exhibit->slug) . '&theme=' . $currentExhibit->theme; ?>"><?php echo ve_translate("items-browse", "Browse items");?>
+				</a>
+			</li>
 
-<div id="footer">
-    <div class="container_16" id="bottom-navigation"">
-        <div class="grid_5">
-           <ul class="navigation">
-               <li>
-                   <a href="<?php echo uri('contact');?>"><?php echo ve_translate('contact', 'Contact');?></a>
-               </li>
-                <?php if (exhibit_builder_get_current_exhibit()): ?>
-                <li>
-                    <a class="return-to" rel="<?php echo uri(); ?>"
-                       href="<?php echo uri('items/browse') . '/?tags=' . ve_get_exhibit_name_from_slug($exhibit->slug) . '&theme=' . $currentExhibit->theme; ?>"><?php echo ve_translate("items-browse", "Browse items");?></a>
-                </li>
+				<?php if ($creditsPage):?>
+				<li>
+					<a  class="return-to" rel="<?php echo uri(); ?>"
+							href="<?php echo uri('credits-' . $eName) . '?theme=' . $currentExhibit->theme;?>"><?php echo ve_translate('credits', 'Credits');?>
+					</a>
+				</li>
 
-                <?php if ($creditsPage):?>
+			<?php endif; ?>
+			<?php endif; ?>
+			<li>
+				<a href="<?php echo uri('about-exhibitions');?>"><?php echo ve_translate("about-exhibitions", "About Exhibitions");?></a>
+			</li>
+		</ul>
+	</div>
+</div>
 
-                <li>
-                    <a  class="return-to" rel="<?php echo uri(); ?>"
-                        href="<?php echo uri('credits-' . $eName) . '?theme=' . $currentExhibit->theme;?>"><?php echo ve_translate('credits', 'Credits');?>
-                    </a>
-                </li>
-
-                <?php endif; ?>
-                <?php endif; ?>
-                 <li>
-                  <a
-                      href="<?php echo uri('about-exhibitions');?>"><?php echo ve_translate("about-exhibitions", "About Exhibitions");?>
-                   </a>
-               </li>
-                
-                 </ul>
-
-
-        </div>
-        <div class="grid_11" style="text-align: right;">
-			<?php getAddThisStandard(); ?>
-        </div>
-    </div>
 </div>
 
 <?php if (isset($_GET['theme'])): ?>
