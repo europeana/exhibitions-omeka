5f7d16010a073f2222649ae1015ea7329db8740c
andyjmaclean@gmail.com <andyjmaclean@gmail.com>
2012-05-16 14:42:28 +0000

    Static version of index_EN simple page

--- webtree/static/index_en.php
+++ webtree/static/index_en.php
@@ -11,17 +11,18 @@ img{
 }
 </style>
 
-<div class="container">
+<div class="container" style="margin-top:15px;">
 
 <div id="header" class="row">
     <div class="twelve columns" style="border-bottom: 1px solid #666666;">
 
 		<a href="http://exhibitions.europeana.eu" title="Click here to return to the Exhibitions start page" style="background:white;"><img src="splash/img/landscape-logo.png" class="main-logo" title="Europeana Exhibitions"></a>
-		<div id="standard_shares" style="position:relative; bottom: 0px; margin-top:-20px;">
-			<a href="http://feeds.feedburner.com/Europeana-Exhibitions" title="Click here to subscribe to the exhibitions RSS feed" style="float:right;"><img src="splash/img/rss_icon-small.gif" class="share" title=""></a>
-			<?php echo getAddThisStandard(); ?>
+		<div id="standard_shares" style="position:absolute; bottom: 3px; right:30px;">
+			<?php echo getAddThisMobile('float:right; display:inline;'); ?>
 		</div>
-
+		<div style="position:absolute; bottom: -2px; right:0px;">
+			<a href="http://feeds.feedburner.com/Europeana-Exhibitions" title="Click here to subscribe to the exhibitions RSS feed" style="float:right;"><img src="splash/img/rss_icon.gif" class="share" title=""></a>
+		</div>
 	</div>
 </div>
 
@@ -32,14 +33,12 @@ img{
 
 <div class="row">
 <div class="twelve columns footer_row">
-
 	<!-- Footer -->
 	<footer>
-		<ul>
-			<li><a href="contact">Contact</a></li>
-			<li><a href="about-exhibitions">About Exhibitions</a></li>
-			<li><a href="http://europeana.eu">Visit Europeana</a></li>
-		</ul>
+		<div class="wrap_at_320"><a href="contact">Contact</a></div>
+		<div class="wrap_at_320"><a href="about-exhibitions">About Exhibitions</a></div>
+		<div class="wrap_at_320"><a href="http://europeana.eu">Visit Europeana</a></div>
+
 	</footer>
 </div>
 </div>
