37a486043c8f786d2c2c889fc70cf9e498fe362b
andyjmaclean@gmail.com <andyjmaclean@gmail.com>
2012-07-11 09:03:14 +0000

    Test oembed

--- webtree/plugins/Tracking/views/public/test/index.php
+++ webtree/plugins/Tracking/views/public/test/index.php
@@ -1,32 +1,21 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
 <?php
-		// helper function
-		function selfURL(){
-			if(!isset($_SERVER['REQUEST_URI'])){
-				$serverrequri = $_SERVER['PHP_SELF'];
-			}
-			else{
-				$serverrequri = $_SERVER['REQUEST_URI'];
-			}
-			$s = empty($_SERVER["HTTPS"]) ? '' : ($_SERVER["HTTPS"] == "on") ? "s" : "";
-			$protocol = strleft(strtolower($_SERVER["SERVER_PROTOCOL"]), "/").$s;
-			$port = ($_SERVER["SERVER_PORT"] == "80") ? "" : (":".$_SERVER["SERVER_PORT"]);
-			return $protocol."://".$_SERVER['SERVER_NAME'].$port.$serverrequri;
-		}
-		
+		$url="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+		$url=urlencode($url);
 ?>
+
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
         <title>jquery-oembed link replace example</title>
         <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
     	<link rel="alternate" type="application/json+oembed"
-    		href="<?php echo(WEB_ROOT); ?>/service/oembed/8?url=<?php urlencode(selfURL);?>;format=json"
+    		href="<?php echo(WEB_ROOT); ?>/service/oembed/8?url=<?php echo($url); ?>;&format=json"
     		title="Andy's test title json" />
     	
     	<link rel="alternate" type="text/xml+oembed"
-    		href="<?php echo(WEB_ROOT); ?>/service/oembed/8?url=http%3a%2f%2facceptance.exhibit.eanadev.org%2ftrack_embed%2fdownload%2f185&amp;format=xml"
+    		href="<?php echo(WEB_ROOT); ?>/service/oembed/8?url=<?php echo($url); ?>;&format=xml"
     		title="Andy's test title xml" />
 
     	
@@ -34,6 +23,13 @@
 </head>
 <body>
 
+
+<?php
+		$url="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+		$url=urlencode($url);
+?>
+		
+		
 <br>
 public/test/index
 <br>
