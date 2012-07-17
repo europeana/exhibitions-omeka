7c3f379dc3e3b00af6fafe6dcde10e9d68c11e02
andyjmaclean@gmail.com <andyjmaclean@gmail.com>
2012-07-17 14:13:41 +0000

    Oembed updates

--- webtree/plugins/Tracking/controllers/OembedController.php
+++ webtree/plugins/Tracking/controllers/OembedController.php
@@ -108,6 +108,9 @@ class Tracking_OembedController extends Omeka_Controller_Action
                		if($oembedFieldName == "provider_name"){
                   		$jsonPair = array('"' . $oembedFieldName . '"', '"Europeana; ' . $val . '"');                		
                 	}
+               		elseif($oembedFieldName == "title"){
+                  		$jsonPair = array('"' . $oembedFieldName . '"', '"faketitle--' . $val . '"');                		
+                	}
                 	else{
                   		$jsonPair = array('"' . $oembedFieldName . '"', '"' . $val . '"');                		
                 	}
