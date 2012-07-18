61a674677c22ef6da053b5cd2ed684fc74208126
andyjmaclean@gmail.com <andyjmaclean@gmail.com>
2012-07-18 09:04:06 +0000

    Oembed updates

--- webtree/plugins/Tracking/controllers/OembedController.php
+++ webtree/plugins/Tracking/controllers/OembedController.php
@@ -241,6 +241,8 @@ class Tracking_OembedController extends Omeka_Controller_Action
             // changed provider_url
             // added thumbnail dimensions
             // removed accented 'a'
+            // matched provider url to test
+            /*
             echo    '{"title":"Slow and quick csardasy",'.
 					'"description":"Slow and quick csardasy - most entertaining dances are dances for couples,",'.
 					'"author":"DEF EMBED FIELD--Performed by unknown dancers",'.
@@ -252,9 +254,17 @@ class Tracking_OembedController extends Omeka_Controller_Action
 					'"html":"\u003ciframe src=\"http:\/\/127.0.0.1\/ombad\/webtree\/track_embed\/download\/269\" '.
 					' frameborder=\"0\" '.
 					' allowfullscreen\u003e\u003c\/iframe\u003e"} ';
+	*/
 
+        	$youtube =  '{"provider_url": "http:\/\/www.youtube.com\/", ' . 
+            			 '"thumbnail_url": "http:\/\/i2.ytimg.com\/vi\/M3r2XDceM6A\/hqdefault.jpg", ' .
+            			 '"title": "Amazing Nintendo Facts", ' .
+            			 '"html": "\u003ciframe width=\"480\" height=\"270\" src=\"http:\/\/www.youtube.com\/embed\/M3r2XDceM6A?fs=1\u0026feature=oembed\" frameborder=\"0\" allowfullscreen\u003e\u003c\/iframe\u003e", ' .
+            			 '"author_name": "ZackScott", "height": 270, "thumbnail_width": 480, "width": 480, "version": "1.0", ' .
+            			 '"author_url": "http:\/\/www.youtube.com\/user\/ZackScott", ' .
+            			 '"provider_name": "YouTube", "type": "video", "thumbnail_height": 360}';
 
-
+        	echo $youtube;
             
             
             
