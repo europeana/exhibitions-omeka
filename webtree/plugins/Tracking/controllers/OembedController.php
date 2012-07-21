032fe52f47aca63193c610ef497f0b66cf4a3a9b
andyjmaclean@gmail.com <andyjmaclean@gmail.com>
2012-07-21 15:48:09 +0000

    Oembed  - clear floats on metadata

--- webtree/plugins/Tracking/controllers/OembedController.php
+++ webtree/plugins/Tracking/controllers/OembedController.php
@@ -214,7 +214,7 @@ class Tracking_OembedController extends Omeka_Controller_Action
 	        			.				'<img width="100%" src="' . WEB_ROOT . '/track_embed/download/' . $item->id . '"/>'
 	        			.			'</a>'
 	        			
-	        			.			'<div style="float:left; position: relative; top: -2.5em; margin-left: 1em; font-size:1.2em; font-weight:bold;">'
+	        			.			'<div style="float:left; clear:both; position: relative; top: -2.5em; margin-left: 1em; font-size:1.2em; font-weight:bold;">'
 	        			.  				$finalRightsValHtml
 	        			.			'</div>'
 	        			.		'</div>';
