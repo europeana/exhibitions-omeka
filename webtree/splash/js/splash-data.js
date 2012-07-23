7806e937a52f5a73d0e1f073807ab94f744f08cd
andyjmaclean@gmail.com <andyjmaclean@gmail.com>
2012-07-23 14:03:36 +0000

    Language link update

--- webtree/splash/js/splash-data.js
+++ webtree/splash/js/splash-data.js
@@ -6,9 +6,9 @@ var splashData = function(){
 				
 				"languages":[
 				             {"langClass":"language-en-main", "link":"exhibits/show/european-sports-en", "label":"English"},
-				     		 {"langClass":"language-es-main", "link":"exhibits/show/patrimonio-deportivo-europeo-e", "label":"Espa&ntilde;ol"},
-				    		 {"langClass":"language-fr-main", "link":"exhibits/show/heritage-du-sport-en-europe-fr", "label":"Fran&ccedil;ais"},
-				    		 {"langClass":"language-de-main", "link":"exhibits/show/europaische-sport-geschichte-d", "label":"Deutsch"}
+				     		 {"langClass":"language-es-main", "link":"exhibits/show/european-sports-es", "label":"Espa&ntilde;ol"},
+				    		 {"langClass":"language-fr-main", "link":"exhibits/show/european-sports-fr", "label":"Fran&ccedil;ais"},
+				    		 {"langClass":"language-de-main", "link":"exhibits/show/european-sports-de", "label":"Deutsch"}
 				             ]   
 
 		}; 
