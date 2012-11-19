6034fd214490e6e33495c728661aef8aad1b242e
dean.birkett@kb.nl <dean.birkett@kb.nl>
2012-11-19 09:29:12 +0000

    Including the Royal Book Collections exhibition

--- webtree/splash/js/splash-data.js
+++ webtree/splash/js/splash-data.js
@@ -1,8 +1,20 @@
 var splashData = function(){
 
+		/* royal book collections */
+
+		var sports = { "contextmenu" : "royalbookcollections", "featured": true, "img": "splash/img/royalbookcollections.jpg", "overlay": "splash/logos/logo-europeana-regia.png", "name": "Royal Book Collections", "menu":{ "label" : "Share on...", "icon": "splash/img/share_icon.gif", "items":[{"label": "Twitter", "icon": "splash/img/twitter_icon.gif", "goto": "//twitter.com/intent/tweet?text=Royal Book Collections: http://exhibitions.europeana.eu/exhibits/show/royal-book-collections-en" }, {"label": "Facebook", "icon": "splash/img/facebook_icon.gif", "goto": "//facebook.com/sharer/sharer.php?u=http://exhibitions.europeana.eu/exhibits/show/royal-book-collections-en" } ] }, 
+				
+				"languages":[
+				             {"langClass":"language-en-main", "link":"exhibits/show/royal-book-collections-en", "label":"English"},
+				    		 {"langClass":"language-fr-main", "link":"exhibits/show/royal-book-collections-fr", "label":"Fran&ccedil;ais"}
+				             ]   
+
+		}; 
+
+
 		/* sports */
 
-		var sports = { "contextmenu" : "sports", "featured": true, "img": "splash/img/sports.jpg", "overlay": "splash/logos/logo-white.png", "name": "European Sport Heritage", "menu":{ "label" : "Share on...", "icon": "splash/img/share_icon.gif", "items":[{"label": "Twitter", "icon": "splash/img/twitter_icon.gif", "goto": "//twitter.com/intent/tweet?text=European Sports: http://exhibitions.europeana.eu/exhibits/show/european-sports" }, {"label": "Facebook", "icon": "splash/img/facebook_icon.gif", "goto": "//facebook.com/sharer/sharer.php?u=http://exhibitions.europeana.eu/exhibits/show/european-sports-en" } ] }, 
+		var sports = { "contextmenu" : "sports", "featured": false, "img": "splash/img/sports.jpg", "overlay": "splash/logos/logo-white.png", "name": "European Sport Heritage", "menu":{ "label" : "Share on...", "icon": "splash/img/share_icon.gif", "items":[{"label": "Twitter", "icon": "splash/img/twitter_icon.gif", "goto": "//twitter.com/intent/tweet?text=European Sports: http://exhibitions.europeana.eu/exhibits/show/european-sports" }, {"label": "Facebook", "icon": "splash/img/facebook_icon.gif", "goto": "//facebook.com/sharer/sharer.php?u=http://exhibitions.europeana.eu/exhibits/show/european-sports-en" } ] }, 
 				
 				"languages":[
 				             {"langClass":"language-en-main", "link":"exhibits/show/european-sports-en", "label":"English"},
@@ -244,6 +256,7 @@ var splashData = function(){
 			
 			
 		return [
+				royalbookcollections,
 		        sports,
 		        nineteenfourteen,
 		        wikiLoves,
