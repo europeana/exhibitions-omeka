a53c938fbd3609e471f4c3ef5e295d0b62fb54f0
dean.birkett@kb.nl <dean.birkett@kb.nl>
2012-12-06 14:12:29 +0000

    updated splash-data.js to include migration info - Anna needs to supply proper title etc.

--- webtree/splash/js/splash-data.js
+++ webtree/splash/js/splash-data.js
@@ -1,8 +1,19 @@
 var splashData = function(){
 
+			/* dpla migration exhibition */
+
+		var migration = { "contextmenu" : "migration", "featured": false, "img": "splash/img/migration.jpg", "overlay": "splash/logos/logo-white.png", "name": "Royal Book Collections", "menu":{ "label" : "Share on...", "icon": "splash/img/share_icon.gif", "items":[{"label": "Twitter", "icon": "splash/img/twitter_icon.gif", "goto": "//twitter.com/intent/tweet?text=Migration: http://exhibitions.europeana.eu/exhibits/show/migration-en" }, {"label": "Facebook", "icon": "splash/img/facebook_icon.gif", "goto": "//facebook.com/sharer/sharer.php?u=http://exhibitions.europeana.eu/exhibits/show/migration-en" } ] }, 
+				
+				"languages":[
+				             {"langClass":"language-en-main", "link":"exhibits/show/migration-en", "label":"English"},
+				             ]   
+
+		}; 
+
+
 		/* royal book collections */
 
-		var royalbookcollections = { "contextmenu" : "royalbookcollections", "featured": true, "img": "splash/img/royalbookcollections.jpg", "overlay": "splash/logos/logo-white.png", "name": "Royal Book Collections", "menu":{ "label" : "Share on...", "icon": "splash/img/share_icon.gif", "items":[{"label": "Twitter", "icon": "splash/img/twitter_icon.gif", "goto": "//twitter.com/intent/tweet?text=Royal Book Collections: http://exhibitions.europeana.eu/exhibits/show/royal-book-collections-en" }, {"label": "Facebook", "icon": "splash/img/facebook_icon.gif", "goto": "//facebook.com/sharer/sharer.php?u=http://exhibitions.europeana.eu/exhibits/show/royal-book-collections-en" } ] }, 
+		var royalbookcollections = { "contextmenu" : "royalbookcollections", "featured": false, "img": "splash/img/royalbookcollections.jpg", "overlay": "splash/logos/logo-white.png", "name": "Royal Book Collections", "menu":{ "label" : "Share on...", "icon": "splash/img/share_icon.gif", "items":[{"label": "Twitter", "icon": "splash/img/twitter_icon.gif", "goto": "//twitter.com/intent/tweet?text=Royal Book Collections: http://exhibitions.europeana.eu/exhibits/show/royal-book-collections-en" }, {"label": "Facebook", "icon": "splash/img/facebook_icon.gif", "goto": "//facebook.com/sharer/sharer.php?u=http://exhibitions.europeana.eu/exhibits/show/royal-book-collections-en" } ] }, 
 				
 				"languages":[
 				             {"langClass":"language-en-main", "link":"exhibits/show/royal-book-collections-en", "label":"English"},
@@ -256,6 +267,7 @@ var splashData = function(){
 			
 			
 		return [
+				migration,
 				royalbookcollections,
 		        sports,
 		        nineteenfourteen,
