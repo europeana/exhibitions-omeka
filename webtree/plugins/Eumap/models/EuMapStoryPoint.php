64f848f0e46ec21a7db63bf47d6dfd43f9c00d39
andyjmaclean@gmail.com <andyjmaclean@gmail.com>
2013-05-25 19:55:28 +0000

    commented code

--- webtree/plugins/Eumap/models/EuMapStoryPoint.php
+++ webtree/plugins/Eumap/models/EuMapStoryPoint.php
@@ -2,10 +2,12 @@
 
 class EUMapStoryPoint extends Omeka_Record
 {
-    public $hash;
     public $id;
-    public $map_id;
-    public $lat;
-    public $lon;
-    public $page_id;
+
+    public $hash;		/* url hash for auto-open */
+    public $map_id;		/* parent */
+    public $lat;		/* pos */
+    public $lon;		/* pos */
+    public $page_id;	/* object for popup to open */
 }
+
